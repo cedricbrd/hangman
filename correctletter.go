d57db280ad2e6381a08fd75ca0d15abcf91568f6
cedric <cedric.buriand@ynov.com>
2023-11-20 21:26:43 +0100

    add verif function

---
+++ correctletter.go
@@ -0,0 +1,15 @@
+package hangman
+
+import "strings"
+
+func correctLetter(correctLetters byte, word string) bool {
+	correctLetters = byte(strings.ToLower(string(correctLetters))[0])
+
+	for _, lettreMot := range word {
+		lettreMot = rune(strings.ToLower(string(lettreMot))[0])
+		if lettreMot == rune(correctLetters) {
+			return true
+		}
+	}
+	return false
+}
